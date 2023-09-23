2e1b9ccb6d55b7ababefc8fc7d03932f9459034e
MeenaRishitha <143867697+MeenaRishitha@users.noreply.github.com>
2023-09-23 12:52:24 +0530

    Update ListOfPrimeorNot1.java

--- ListOfPrimeorNot1.java
+++ ListOfPrimeorNot1.java
@@ -16,8 +16,6 @@ public class Main {
         } 
      if (!flag)
       System.out.println(i + " is a prime number.");
-    else
-      System.out.println(i + " is not a prime number.");
     }
   }
 }
